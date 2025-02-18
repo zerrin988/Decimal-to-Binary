71251a90dc257d3e9f9293a5fb7affc295de0ae9
Zerrin ul haq. Siya <134535520+zerrin988@users.noreply.github.com>
2025-02-19 00:26:58 +0100

    Update decimal to binary.c

--- decimal to binary.c
+++ decimal to binary.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 int main()
 {
-	int i ;
-	int counter =0;
+	int i;
+	int counter = 0;
 	int result [5];
 	int remainder;
-	int input =10;
+	int input = 10;
 	while(input > 0)
 	{
 		remainder = input % 2;
-		result[counter] = remainder ;
-		input /= 2 ;
-		counter++ ;   
+		result[counter] = remainder;
+		input /= 2;
+		counter++;   
 	}
-    	for(i=counter;i > 0;i--)
+    	for(i = counter; i > 0; i--)
     	{
-        		printf("%d",result[i-1]);
+        		printf("%d", result[i - 1]);
     	}
 }
